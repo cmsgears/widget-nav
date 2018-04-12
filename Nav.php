ffbe8ff6740af6b595776cc848949f414b1a0122
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-04-12 07:45:21 +0530

    Renamed BasicNav to Nav.

--- Nav.php
+++ Nav.php
@@ -1,13 +1,28 @@
 <?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
 namespace cmsgears\widgets\nav;
 
-use \Yii;
-use yii\base\Widget;
+// Yii Imports
+use Yii;
 use yii\base\InvalidConfigException;
-use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
-class BasicNav extends \cmsgears\core\common\base\Widget {
+// CMG Imports
+use cmsgears\core\common\base\Widget;
+
+/**
+ * Nav widget show the menu.
+ *
+ * @since 1.0.0
+ */
+class Nav extends Widget {
 
 	// Variables ---------------------------------------------------
 
